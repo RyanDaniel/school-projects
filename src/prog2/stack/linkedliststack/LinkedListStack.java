03efa7c006af2ac296889b45befc57189e0be43d
RyanDaniel <36544031+RyanDaniel@users.noreply.github.com>
2018-02-16 17:41:01 +0100

    Create LinkedListStack.java

---
+++ src/prog2/stack/linkedliststack/LinkedListStack.java
@@ -0,0 +1,61 @@
+package prog2.stack.linkedliststack;
+
+class Stack {
+
+  private Node top;
+
+  public Stack() {
+    top = null;
+  }
+
+  public boolean empty() {
+    return top == null;
+  }
+
+  public void push(int value) {
+    top = new Node(value, top);
+  }
+
+  public int peek() {
+    return top.getValue();
+  }
+
+  public int pop() {
+    int temp = top.getValue();
+    top = top.getNext();
+    return temp;
+  }
+
+  private class Node {
+
+    private final int value;
+    private Node next;
+
+    public Node(int value, Node next) {
+      this.value = value;
+      this.next = next;
+    }
+
+    public int getValue() {
+      return value;
+    }
+
+    public Node getNext() {
+      return next;
+    }
+
+    public void setNext(Node next) {
+      this.next = next;
+    }
+
+  }
+
+}
+
+public class LinkedListStack {
+
+  public static void main(String[] args) {
+    // TO DO
+  }
+
+}
